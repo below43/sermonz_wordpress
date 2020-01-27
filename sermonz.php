594461fd92068a7d25c1fd9b8248db5d250e3d89
andrew <andrew@below43.com>
2020-01-27 21:48:23 +1300

    Initial Commit

---
+++ sermonz.php
@@ -0,0 +1,10 @@
+<?php
+   /*
+   Plugin Name: Sermo.nz
+   Plugin URI: http://sermo.nz
+   description: Sermo.nz Library Plugin
+   Version: 1.0
+   Author: Andrew Drake
+   Author URI: http://andrew.drake.nz
+   License: GPL3
+   */
